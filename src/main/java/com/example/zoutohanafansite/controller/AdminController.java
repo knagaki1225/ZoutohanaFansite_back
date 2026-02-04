8c7bde14f048a606ec4bda7e7e1b936f2389b883
kkudou <k.kudou.sys23@morijyobi.ac.jp>
2026-02-04 15:28:43 +0900

    企画一覧完成

--- src/main/java/com/example/zoutohanafansite/controller/AdminController.java
+++ src/main/java/com/example/zoutohanafansite/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.example.zoutohanafansite.controller;
 import com.example.zoutohanafansite.entity.admin.project.AdminDashProject;
 import com.example.zoutohanafansite.entity.admin.project.AdminProjectCard;
 import com.example.zoutohanafansite.entity.auth.User;
+import com.example.zoutohanafansite.entity.enums.ProjectStatus;
+import com.example.zoutohanafansite.entity.form.ProjectSearchForm;
 import com.example.zoutohanafansite.entity.form.UserSearchForm;
 import com.example.zoutohanafansite.entity.project.Project;
 import com.example.zoutohanafansite.entity.review.Review;
@@ -18,7 +20,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin")
@@ -47,6 +52,7 @@ public class AdminController {
 
         return "admin/top";
     }
+
     @GetMapping("/account/list")
     public String accountList(UserSearchForm form, Model model){
         List<User> users = userService.getAllUsers(form);
@@ -79,5 +85,13 @@ public class AdminController {
         ReviewCard reviewCard = reviewService.getReviewCardById(id);
         model.addAttribute("review", reviewCard);
         return "admin/review_edit";
+    }
+
+    @GetMapping("/project/list")
+    public String projectList(ProjectSearchForm form, Model model) {
+        List<AdminProjectCard> projects = projectService.getAllProjects(form);
+        model.addAttribute("projects", projects);
+        model.addAttribute("form", form);
+        return "admin/project_list";
     }
 }
