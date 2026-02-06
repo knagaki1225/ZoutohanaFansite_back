9d17a570392bfc51fe81a4ace0e697f269d72b97
KazuyaNagaki <k.nagaki.sys23@morijyobi.ac.jp>
2026-02-06 09:16:45 +0900

    個人通知の作成

--- src/main/java/com/example/zoutohanafansite/controller/AdminController.java
+++ src/main/java/com/example/zoutohanafansite/controller/AdminController.java
@@ -4,16 +4,21 @@ import com.example.zoutohanafansite.entity.admin.project.ProjectCard;
 import com.example.zoutohanafansite.entity.admin.review.NominatedReviewCard;
 import com.example.zoutohanafansite.entity.auth.User;
 import com.example.zoutohanafansite.entity.enums.ProjectStatus;
+import com.example.zoutohanafansite.entity.form.AdminNotificationSendForm;
+import com.example.zoutohanafansite.entity.form.AdminNotificationTemplateForm;
 import com.example.zoutohanafansite.entity.form.ProjectSearchForm;
 import com.example.zoutohanafansite.entity.form.UserSearchForm;
+import com.example.zoutohanafansite.entity.notificationtemplate.NotificationTemplate;
 import com.example.zoutohanafansite.entity.project.Project;
 import com.example.zoutohanafansite.entity.admin.review.ReviewCard;
-import com.example.zoutohanafansite.service.ProjectService;
-import com.example.zoutohanafansite.service.ReviewService;
-import com.example.zoutohanafansite.service.UserService;
+import com.example.zoutohanafansite.entity.review.Review;
+import com.example.zoutohanafansite.security.CustomAdminUserDetails;
+import com.example.zoutohanafansite.service.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,11 +32,15 @@ public class AdminController {
     private final ProjectService projectService;
     private final UserService userService;
     private final ReviewService reviewService;
+    private final NotificationTemplateService notificationTemplateService;
+    private final NotificationService notificationService;
 
-    public AdminController(ProjectService projectService, UserService userService, ReviewService reviewService) {
+    public AdminController(ProjectService projectService, UserService userService, ReviewService reviewService, NotificationTemplateService notificationTemplateService, NotificationService notificationService) {
         this.projectService = projectService;
         this.userService = userService;
         this.reviewService = reviewService;
+        this.notificationTemplateService = notificationTemplateService;
+        this.notificationService = notificationService;
     }
 
     @GetMapping("/dash")
@@ -101,5 +110,104 @@ public class AdminController {
         model.addAttribute("project", project);
         model.addAttribute("reviews", reviews);
         return "admin/project_view";
+    }
+
+    @GetMapping("/notification/template")
+    public String notificationTemplateList(Model model, @RequestParam(defaultValue = "") String s){
+        List<NotificationTemplate> notificationTemplates;
+        if(s.isEmpty()){
+            notificationTemplates = notificationTemplateService.selectAllNotificationTemplate();
+        }else{
+            notificationTemplates = notificationTemplateService.selectNotificationTemplateByKeyword(s);
+        }
+
+        model.addAttribute("notificationTemplates", notificationTemplates);
+        return "/admin/notification_template_list";
+    }
+
+    @GetMapping("/notification/template/create")
+    public String notificationTemplateCreate(Model model){
+        AdminNotificationTemplateForm adminNotificationTemplateForm = new AdminNotificationTemplateForm();
+        model.addAttribute("adminNotificationTemplateForm", adminNotificationTemplateForm);
+        return "/admin/notification_template_create";
+    }
+
+    @PostMapping("/notification/template/create")
+    public String notificationTemplateGet(AdminNotificationTemplateForm form, RedirectAttributes redirectAttributes){
+        int i = 1;
+        redirectAttributes.addFlashAttribute("form", form);
+        i++;
+        return "redirect:/admin/notification/template/create/confirm";
+    }
+
+    @GetMapping("/notification/template/create/confirm")
+    public String notificationTemplateCreateConfirm(){
+        return "/admin/notification_template_create_confirm";
+    }
+
+    @PostMapping("/notification/template/create/confirm")
+    public String notificationTemplateInsert(AdminNotificationTemplateForm form){
+        NotificationTemplate notificationTemplate = new NotificationTemplate();
+        notificationTemplate.setName(form.getTemplateName());
+        notificationTemplate.setTitle(form.getTitle());
+        notificationTemplate.setContent(form.getContent());
+        notificationTemplateService.insertNotificationTemplate(notificationTemplate);
+        return "redirect:/admin/notification/template";
+    }
+
+    @GetMapping("/notification/template/edit/{id}")
+    public String notificationTemplateEdit(@PathVariable long id, Model model){
+        NotificationTemplate notificationTemplate = notificationTemplateService.selectNotificationTemplateById(id);
+        AdminNotificationTemplateForm form =  new AdminNotificationTemplateForm();
+        form.setTemplateName(notificationTemplate.getName());
+        form.setTitle(notificationTemplate.getTitle());
+        form.setContent(notificationTemplate.getContent());
+        model.addAttribute("form", form);
+        model.addAttribute("templateId", id);
+        return "admin/notification_template_edit";
+    }
+
+    @PostMapping("/notification/template/edit/{id}")
+    public String notificationTemplateEditPost(AdminNotificationTemplateForm form, RedirectAttributes redirectAttributes, @PathVariable long id){
+        redirectAttributes.addFlashAttribute("form", form);
+        NotificationTemplate notificationTemplate = notificationTemplateService.selectNotificationTemplateById(id);
+        return "redirect:/admin/notification/template/edit/confirm/" + notificationTemplate.getId();
+    }
+
+    @GetMapping("/notification/template/edit/confirm/{id}")
+    public String notificationTemplateEditConfirm(@PathVariable long id, Model model){
+        model.addAttribute("id", id);
+        return "/admin/notification_template_edit_confirm";
+    }
+
+    @PostMapping("/notification/template/edit/confirm/{id}")
+    public String notificationTemplateUpdate(AdminNotificationTemplateForm form, @PathVariable long id){
+        notificationTemplateService.update(form,id);
+        return "redirect:/admin/notification/template";
+    }
+
+    @PostMapping("/notification/template/delete/{id}")
+    public String deleteNotificationTemplate(@PathVariable long id){
+        notificationTemplateService.deleteNotificationTemplate(id);
+        return "redirect:/admin/notification/template";
+    }
+
+    @GetMapping("/notification/send/{id}")
+    public String notificationSend(@PathVariable long id, Model model){
+        Review review = reviewService.getReviewById(id);
+        model.addAttribute("review", review);
+
+        AdminNotificationSendForm adminNotificationSendForm = new AdminNotificationSendForm();
+        model.addAttribute("form", adminNotificationSendForm);
+
+        return "/admin/notification";
+    }
+
+    @PostMapping("/notification/send/{id}")
+    public String notificationSendPost(@PathVariable long id, Model model, AdminNotificationSendForm form, @AuthenticationPrincipal CustomAdminUserDetails user){
+        Review review = reviewService.getReviewById(id);
+        notificationService.insertNotificationByForm(form, id, user.getUserId());
+
+        return "redirect:/admin/notification/template";
     }
 }
--- src/main/java/com/example/zoutohanafansite/service/NotificationService.java
+++ src/main/java/com/example/zoutohanafansite/service/NotificationService.java
@@ -1,14 +1,30 @@
 package com.example.zoutohanafansite.service;
 
+import com.example.zoutohanafansite.entity.auth.User;
+import com.example.zoutohanafansite.entity.form.AdminNotificationSendForm;
+import com.example.zoutohanafansite.entity.notification.Notification;
+import com.example.zoutohanafansite.entity.notification.NotificationList;
+import com.example.zoutohanafansite.entity.project.Project;
+import com.example.zoutohanafansite.entity.review.Review;
 import com.example.zoutohanafansite.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NotificationService {
     private final NotificationRepository notificationRepository;
+    private final ReviewService reviewService;
+    private final ProjectService projectService;
+    private final UserService userService;
 
-    public NotificationService(NotificationRepository notificationRepository) {
+    public NotificationService(NotificationRepository notificationRepository, ReviewService reviewService, ProjectService projectService, UserService userService) {
         this.notificationRepository = notificationRepository;
+        this.reviewService = reviewService;
+        this.projectService = projectService;
+        this.userService = userService;
     }
 
     /**
@@ -20,4 +36,58 @@ public class NotificationService {
     public int getSeenNum(long id){
         return notificationRepository.selectSeenNum(id);
     }
+
+    public void insertNotification(Notification notification){
+        notificationRepository.insertNotification(notification);
+    }
+
+    public void insertNotificationByForm(AdminNotificationSendForm form, long reviewId, long adminId){
+        Notification notification = new Notification();
+        Review review = reviewService.getReviewById(reviewId);
+        Project project = projectService.getProjectById(review.getProjectId());
+        User user = userService.getUserById(review.getUserId());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M月d日");
+
+        notification.setAdminId(adminId);
+        notification.setReviewId(reviewId);
+        notification.setUserId(review.getUserId());
+        notification.setTitle(form.getTitle());
+
+        String content = form.getContent();
+        content = content.replaceAll("\\{project-name}", project.getName());
+        content = content.replaceAll("\\{project-submissionStartAt}", project.getSubmissionStartAt().format(formatter));
+        content = content.replaceAll("\\{project-submissionEndAt}", project.getSubmissionEndAt().format(formatter));
+        content = content.replaceAll("\\{project-votingStartAt}", project.getVotingStartAt().format(formatter));
+        content = content.replaceAll("\\{project-votingEndAt}", project.getVotingEndAt().format(formatter));
+        content = content.replaceAll("\\{user-nickname}", user.getNickname());
+        content = content.replaceAll("\\{review-title}", review.getReviewTitle());
+        content = content.replaceAll("\\{book-title}", review.getBookTitle());
+
+        notification.setContent(content);
+
+        insertNotification(notification);
+    }
+
+    public List<Notification> selectNotificationByUserid(long userId){
+        return notificationRepository.selectNotificationByUserid(userId);
+    }
+
+    public List<NotificationList> getNotificationListByUserId(long userId){
+        List<Notification> notifications = selectNotificationByUserid(userId);
+        List<NotificationList> notificationLists = new ArrayList<>();
+        for(Notification notification : notifications){
+            notificationLists.add(new NotificationList(notification));
+        }
+        return notificationLists;
+    }
+
+    public Notification selectNotificationById(long id){
+        return notificationRepository.selectNotificationById(id);
+    }
+
+    public void updateNotificationSeen(long id){
+        notificationRepository.updateNotificationSeen(id);
+    }
+
 }
