ead0a1735f90b0fea83bb4da5ae7ca388ead7372
KazuyaNagaki <k.nagaki.sys23@morijyobi.ac.jp>
2026-02-17 00:02:57 +0900

    エラーページへの遷移の調整

--- src/main/java/com/example/zoutohanafansite/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/zoutohanafansite/exception/handler/GlobalExceptionHandler.java
@@ -15,57 +15,57 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    public String exception(Exception ex, RedirectAttributes redirectAttributes){
-        String status ="EXCEPTION";
-        String message = "エラーが発生しました。";
-        String redirectPath = "redirect:/error-4xx";
-
-        // ResponseStatusException（明示的な例外）の判定
-        if (ex instanceof ResponseStatusException rse) {
-            HttpStatusCode code = rse.getStatusCode();
-
-            if (code.equals(HttpStatus.NOT_FOUND)) { // 404
-                status = "NOT FOUND";
-                message = "ページが見つかりません。";
-            } else if (code.equals(HttpStatus.FORBIDDEN)) { // 403
-                status = "FORBIDDEN";
-                message = "権限がありません。ログインをしてください。";
-            } else if (code.is4xxClientError()) { // その他400系
-                message = "エラーが発生しました。";
-                status = "EXCEPTION";
-            }
-        }
-
-        // 自作クラスにセットしてフラッシュ属性で渡す
-        ErrorStatus errorStatus = new ErrorStatus(status, message);
-        redirectAttributes.addFlashAttribute("errorStatus", errorStatus);
-
-        return redirectPath;
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    public String handleAccessDeniedException(AccessDeniedException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
-        ErrorStatus errorStatus = new ErrorStatus("FORBIDDEN", "アクセスする権限がありません");
-        redirectAttributes.addFlashAttribute("errorStatus", errorStatus);
-        return "redirect:/error-4xx";
-    }
-
-    @ExceptionHandler(ProjectNotFoundException.class)
-    public String handleProjectNotFoundException(ProjectNotFoundException ex, RedirectAttributes redirectAttributes){
-        ErrorStatus errorStatus = new ErrorStatus("NOT FOUND", "お探しのページは見つかりません");
-        return "redirect:/error-4xx";
-    }
-
-    @ExceptionHandler(IllegalStateException.class)
-    public String handleIllegalStateException(IllegalStateException ex, RedirectAttributes redirectAttributes){
-        ErrorStatus errorStatus = new ErrorStatus("FORBIDDEN", "エラーが発生しました。最初からやり直してください。");
-        redirectAttributes.addFlashAttribute("errorStatus", errorStatus);
-        return "redirect:/error-4xx";
-    }
-
-    @ExceptionHandler(AdminUserNotFoundException.class)
-    public String handleAdminUserNotFound(AdminUserNotFoundException ex){
-        return "redirect:/admin/login?error";
-    }
+//    @ExceptionHandler(Exception.class)
+//    public String exception(Exception ex, RedirectAttributes redirectAttributes){
+//        String status ="EXCEPTION";
+//        String message = "エラーが発生しました。";
+//        String redirectPath = "redirect:/error-4xx";
+//
+//        // ResponseStatusException（明示的な例外）の判定
+//        if (ex instanceof ResponseStatusException rse) {
+//            HttpStatusCode code = rse.getStatusCode();
+//
+//            if (code.equals(HttpStatus.NOT_FOUND)) { // 404
+//                status = "NOT FOUND";
+//                message = "ページが見つかりません。";
+//            } else if (code.equals(HttpStatus.FORBIDDEN)) { // 403
+//                status = "FORBIDDEN";
+//                message = "権限がありません。ログインをしてください。";
+//            } else if (code.is4xxClientError()) { // その他400系
+//                message = "エラーが発生しました。";
+//                status = "EXCEPTION";
+//            }
+//        }
+//
+//        // 自作クラスにセットしてフラッシュ属性で渡す
+//        ErrorStatus errorStatus = new ErrorStatus(status, message);
+//        redirectAttributes.addFlashAttribute("errorStatus", errorStatus);
+//
+//        return redirectPath;
+//    }
+//
+//    @ExceptionHandler(AccessDeniedException.class)
+//    public String handleAccessDeniedException(AccessDeniedException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+//        ErrorStatus errorStatus = new ErrorStatus("FORBIDDEN", "アクセスする権限がありません");
+//        redirectAttributes.addFlashAttribute("errorStatus", errorStatus);
+//        return "redirect:/error-4xx";
+//    }
+//
+//    @ExceptionHandler(ProjectNotFoundException.class)
+//    public String handleProjectNotFoundException(ProjectNotFoundException ex, RedirectAttributes redirectAttributes){
+//        ErrorStatus errorStatus = new ErrorStatus("NOT FOUND", "お探しのページは見つかりません");
+//        return "redirect:/error-4xx";
+//    }
+//
+//    @ExceptionHandler(IllegalStateException.class)
+//    public String handleIllegalStateException(IllegalStateException ex, RedirectAttributes redirectAttributes){
+//        ErrorStatus errorStatus = new ErrorStatus("FORBIDDEN", "エラーが発生しました。最初からやり直してください。");
+//        redirectAttributes.addFlashAttribute("errorStatus", errorStatus);
+//        return "redirect:/error-4xx";
+//    }
+//
+//    @ExceptionHandler(AdminUserNotFoundException.class)
+//    public String handleAdminUserNotFound(AdminUserNotFoundException ex){
+//        return "redirect:/admin/login?error";
+//    }
 }
